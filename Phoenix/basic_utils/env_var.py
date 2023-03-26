de21bafeb27b8556b62de87fa727f7ab963a231b
FrederichRiver <friederich@163.com>
2023-03-26 15:58:17 +0800

    envirement variables

---
+++ Phoenix/basic_utils/env_var.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+global environment varibles
+
+"""
+
+PYTHON_VERSION = 3.8
+LOCAL_TIME_ZONE = 'Beijing'
+PROG_NAME = 'Neutrino'
+TIME_FMT = '%Y-%m-%d'
+LOG_FILE = '/opt/neutrino/log/neutrino.log'
+LOG_TIME_FMT = "%Y-%m-%d %H:%M:%S"
+GITHUB_URL = "https://github.com/FrederichRiver/neutrino3"
+EMAIL = "hezhiyuan_tju@163.com"
